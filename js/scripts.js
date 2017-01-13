ebb4573e43a79f3a2216808ed304078704542551
philip putnam <putnamp989@gmail.com>
2017-01-13 13:20:38 -0800

    add jQuery variables and output based on user input for form questions

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,17 @@ $(document).ready(function() {
     var nameInput = $("input#name").val();
     var question1Input = $("input:radio[name=question1]:checked").val();
     var question2Input = $("#question2").val();
+    var question3Input = $("#question3").val();
+    var question4Input = $("input:radio[name=question4]:checked").val();
+    var question5Input = $("#question5").val();
+
 
     $(".name").text(nameInput);
     $(".question1").text(question1Input);
     $(".question2").text(question2Input);
+    $(".question3").text(question3Input);
+    $(".question4").text(question4Input);
+    $(".question5").text(question5Input);
 
     $("#blank").removeClass();
     $("#blank").hide();
