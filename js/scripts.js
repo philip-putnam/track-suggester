b2a5eebc500b67a770204677e8739f29bf8f9aed
philip putnam <putnamp989@gmail.com>
2017-01-13 13:12:45 -0800

    add jQuery in scripts.js for questions 1 and 2, output results

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   $(".btn#submitForm").click(function(event) {
     var nameInput = $("input#name").val();
+    var question1Input = $("input:radio[name=question1]:checked").val();
+    var question2Input = $("#question2").val();
 
     $(".name").text(nameInput);
+    $(".question1").text(question1Input);
+    $(".question2").text(question2Input);
 
     $("#blank").removeClass();
     $("#blank").hide();
