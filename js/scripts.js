c45dd174215b3e840efaccc61d167b0600e26f0b
philip putnam <putnamp989@gmail.com>
2017-01-13 12:57:07 -0800

    add removeClass and addClass to show results and hide form to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
 
     $(".name").text(nameInput);
 
+    $("#blank").removeClass();
+    $("#blank").hide();
+
+    $("#results").removeClass();
+    $("#results").addClass(".shown");
+
 
     event.preventDefault();
   });
